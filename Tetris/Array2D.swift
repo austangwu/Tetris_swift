9d32cd6772b15c0e529435ecb47835e76ec47981
Austin Wu <austin.wu@duke.edu>
2014-11-21 12:29:05 -0500

    second commit 11/21

--- Tetris/Array2D.swift
+++ Tetris/Array2D.swift
@@ -6,4 +6,30 @@
 //  Copyright (c) 2014 Austin Wu. All rights reserved.
 //
 
-import Foundation
+//1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    //2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int){
+        self.columns = columns
+        self.rows = rows
+        
+        //3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil )
+    }
+    
+    //4
+    subscript(column: Int, row: Int) -> T? {
+        get{
+            return array [(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+    
+}
