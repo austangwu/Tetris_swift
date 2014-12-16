f01dff68d3963b20e91ad9bffefc53aea3cedfdf
Austin Wu <austin.wu@duke.edu>
2014-12-15 22:28:33 -0600

    Secon

--- Tetris/Array2D.swift
+++ Tetris/Array2D.swift
@@ -11,18 +11,14 @@ class Array2D<T> {
     let columns: Int
     let rows: Int
     
-    //2
     var array: Array<T?>
     
     init(columns: Int, rows: Int){
         self.columns = columns
         self.rows = rows
-        
-        //3
         array = Array<T?>(count:rows * columns, repeatedValue: nil )
     }
     
-    //4
     subscript(column: Int, row: Int) -> T? {
         get{
             return array [(row * columns) + column]
